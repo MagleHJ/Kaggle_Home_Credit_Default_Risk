e1908e0024c24d7818bc93e414504b2f4c11052d
MagleHJ <maglehj@163.com>
2018-06-26 16:24:03 +0800

    搭建基本框架

--- src/feature_processing.py
+++ src/feature_processing.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-
+import utils
+import config
 # -----------------
 # 特征工程函数
 # 根据具体内容可以进行扩展
 # -----------------
 def feature_engineering(df):
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace= True)
+    df, cat_cols = utils.one_hot_encoder(df, config.NAN_AS_CAtEGORY)
     return df
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 from contextlib import contextmanager
 import time
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 @contextmanager
 def timer(msg):
     t0 = time.time()
@@ -15,3 +16,13 @@ def one_hot_encoder(df, nan_as_category = True, categorical_columns = None):
     df = pd.get_dummies(df, columns= categorical_columns, dummy_na= nan_as_category)
     new_columns = [c for c in df.columns if c not in original_columns]
     return df, new_columns
+   
+# Display/plot feature importance
+def display_importances(feature_importance_df_, path):
+    cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(by="importance", ascending=False)[:40].index
+    best_features = feature_importance_df_.loc[feature_importance_df_.feature.isin(cols)]
+    plt.figure(figsize=(8, 10))
+    sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig(path)
